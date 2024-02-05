890f612f41d45ae2514713ea0c011c2258f61f14
Dhyani <gandhidhyani@gmail.com>
2024-02-05 17:01:59 -0600

    Added logic for request header info

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res){
+  var lang = req.headers['accept-language'];
+  var software = req.headers['user-agent'];
+  //var ip = req.headers.host;
+  res.json({ipaddress: req.socket.remoteAddress, language: lang, software: software});
+});
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
